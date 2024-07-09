6d78253da52f45ec554e29e9579af2867813516d
Taylor Seager <taylorseager718@gmail.com>
2024-07-09 18:46:04 -0500

    save point

--- Program.cs
+++ Program.cs
@@ -1,17 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
+List<string> products = new List<string>()
+{
+    "Football",
+    "Hockey Stick",
+    "Boomerang",
+    "Frisbee",
+    "Golf Putter"
+};
+
 string greeting = @"Welcome to Thrown For a Loop
 Your one-stop shop for used sporting equipment";
 
 Console.WriteLine(greeting);
 
-Console.WriteLine("Please enter a product name: ");
-
-string response = Console.ReadLine().Trim();
-
-while (string.IsNullOrEmpty(response))
+Console.WriteLine("Products:");
+for (int i = 0; i < products.Count; i++)
 {
-    Console.WriteLine("You didn't choose anything, try again!");
-    response = Console.ReadLine().Trim();
+    Console.WriteLine($"{i + 1}. {products[i]}");
+}
+Console.WriteLine("Please enter a product number: ");
+
+int response = int.Parse(Console.ReadLine().Trim());
+
+while (response > products.Count || response < 1)
+{
+    Console.WriteLine("Choose a number between 1 and 5!");
+    response = int.Parse(Console.ReadLine().Trim());
 }
 
 Console.WriteLine($"You chose: {response}");
