fdc9825f6e986c1fc32cceafb30a01b30f50e9eb
Taylor Seager <taylorseager718@gmail.com>
2024-07-09 15:00:05 -0500

    if else statement added

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string greeting = @"Welcome to Thrown For a Loop
 Your one-stop shop for used sporting equipment";
+
 Console.WriteLine(greeting);
+
 Console.WriteLine("Please enter a product name: ");
+
 string response = Console.ReadLine();
-Console.WriteLine(@$"You chose: {response}. 
-Thank you for your input!");
+
+if (string.IsNullOrWhiteSpace(response))
+{
+    Console.WriteLine("You didn't choose anything!");
+}
+else
+{
+    Console.WriteLine($"You chose: {response}");
+}
